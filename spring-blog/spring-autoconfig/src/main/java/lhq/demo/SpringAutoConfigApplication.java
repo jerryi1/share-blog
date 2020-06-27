2fcecb3abbf449e9c93bf44c311e5e115563374a
lihuaqing <lihuaqing@maxtropy.com>
2020-06-27 12:54:46 +0800

    添加spring父子项目

---
+++ spring-blog/spring-autoconfig/src/main/java/lhq/demo/SpringAutoConfigApplication.java
@@ -0,0 +1,11 @@
+package lhq.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAutoConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAutoConfigApplication.class, args);
+    }
+}
